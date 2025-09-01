b25a75b45b857d59284c2bdc4737378468d5e18a
netfosi <netfosi@outlook.com>
2025-09-02 01:51:01 +0800

    fix: 修复无限重定向问题，改为使用hash路由模式

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import { Home, Article, AllArticles, About } from './components/index.js'
 import './styles/index.css'
@@ -12,21 +12,13 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/blog/'),
+  history: createWebHashHistory(),
   routes
 })
 
 const app = createApp(App)
 app.use(router)
 
-// GitHub Pages SPA 路由处理
-if (window.location.pathname.indexOf('/blog/') === 0) {
-  const redirect = window.location.search.match(/redirect=([^&]+)/);
-  if (redirect) {
-    window.history.replaceState(null, null, 
-      window.location.pathname + redirect[1] + window.location.hash
-    );
-  }
-}
+
 
 app.mount('#app')
